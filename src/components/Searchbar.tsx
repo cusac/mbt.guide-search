af81ecdcb339ee9fb1478f6cc45e3247d83722f6
Justin Headley <headley.justin@gmail.com>
2021-02-18 10:39:27 -0800

    feature: search suggestions

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -3,9 +3,12 @@ import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from 'store';
 import { searchSegments, searchYTVideos, setSearchText } from '../store/video/video.store';
 import { toastError } from '../utils/toastHelper';
+import Autosuggest from 'react-autosuggest';
+import { repository } from 'services';
+import { uniq, uniqBy } from 'lodash';
 
 const Searchbar = (): any => {
-  // const [term, setTerm] = React.useState('');
+  const [suggestions, setSuggestions] = React.useState([] as string[]);
 
   const searchType = useSelector((state: RootState) => state.video.searchType);
   const searchText = useSelector((state: RootState) => state.video.searchText);
@@ -13,15 +16,18 @@ const Searchbar = (): any => {
   const dispatch = useAppDispatch();
 
   const handleChange = (event: any) => {
-    // setTerm(event.target.value);
     dispatch(setSearchText({ searchText: event.target.value }));
   };
 
-  const search = async (event: any) => {
+  const onSubmit = async (event: any) => {
     event.preventDefault();
+    search();
+  };
+
+  const search = async (text?: string) => {
     if (searchType === 'segment') {
       try {
-        await dispatch(searchSegments({ term: searchText }));
+        await dispatch(searchSegments({ term: text || searchText }));
       } catch (err) {
         toastError(
           'There was an error fetching segment data. Please refresh the page and try again.',
@@ -30,7 +36,7 @@ const Searchbar = (): any => {
       }
     } else {
       try {
-        await dispatch(searchYTVideos({ term: searchText }));
+        await dispatch(searchYTVideos({ term: text || searchText }));
       } catch (err) {
         toastError(
           'There was an error fetching youtube data. Please refresh the page and try again.',
@@ -46,17 +52,67 @@ const Searchbar = (): any => {
       : 'Search for MBT video segments';
   };
 
+  const formatSuggestions = (suggestions: string[]): string[] => {
+    return uniq(suggestions.map(text => text.toLowerCase().trim()));
+  };
+
+  const getSuggestions = async (value: string): Promise<string[]> => {
+    let {
+      data: { docs },
+    } = await repository.tag.list({ $term: value, $limit: 50, $embed: 'segments' });
+
+    // NOTE: Soon the tag filters and formatting will be accomplished on the backend
+    docs = docs.filter(tag => tag.segments && tag.segments.length > 0);
+
+    return formatSuggestions(docs.map(tag => tag.name));
+  };
+
+  const getSuggestionValue = (suggestion: string) => {
+    return suggestion || '';
+  };
+
+  const renderSuggestion = (suggestion: string) => <span>{suggestion || ''}</span>;
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  const onSuggestionsFetchRequested = async ({ value }: { value: string }) => {
+    setSuggestions(await getSuggestions(value));
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const onSuggestionSelected: Autosuggest.OnSuggestionSelected<string> = async (
+    event,
+    { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }
+  ) => {
+    await dispatch(setSearchText({ searchText: suggestionValue }));
+    search(suggestionValue);
+  };
+
+  const inputProps = {
+    className: 'prompt',
+    placeholder: getPlaceHolder(),
+    type: 'text',
+    onChange: handleChange,
+    value: searchText || '',
+    style: { width: '100%' },
+  };
+
   return (
     <div className="search-bar ui segment">
-      <form onSubmit={search} className="ui form">
+      <form onSubmit={onSubmit} className="ui form">
         <div className="field">
           <div className="ui icon input">
-            <input
-              className="prompt"
-              onChange={handleChange}
-              type="text"
-              placeholder={getPlaceHolder()}
-              value={searchText}
+            <Autosuggest
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={onSuggestionsClearRequested}
+              onSuggestionSelected={onSuggestionSelected}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
             />
             <i className="search icon" />
           </div>
