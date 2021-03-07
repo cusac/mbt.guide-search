30381cf13f81e9fe4aafeb0c1b61912d0c9dfc0f
Justin Headley <headley.justin@gmail.com>
2021-03-06 18:01:01 -0800

    feature: expandable header

--- src/components/SmallHeader.tsx
+++ src/components/SmallHeader.tsx
@@ -68,144 +68,144 @@ const SmallHeader = (): any => {
             />
           </a>
         </div>
-        <Transition visible={visible} animation="fly down" duration={500}>
-          <nav
-            className="navbar-collapse navbar-ex1-collapse collapse in"
-            aria-expanded="true"
-            // style=""
-          >
-            <ul id="menu-main-menu" className="nav navbar-nav navbar-right">
-              <li
-                id="menu-item-6"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-4 current_page_item menu-item-6"
-              >
-                <a href="https://www.my-big-toe.com/" aria-current="page">
-                  Home
-                </a>
-              </li>
-              <li
-                id="menu-item-137"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-137"
-              >
-                <a href="https://www.my-big-toe.com/about/">About</a>
-              </li>
-              <li
-                id="menu-item-9"
-                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"
-              >
-                <a href="/media/">Videos</a>
-              </li>
-              <li
-                id="menu-item-10"
-                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10"
-              >
-                <a href="/store1">Store</a>
-                <ul className="sub-menu">
-                  <li
-                    id="menu-item-142"
-                    className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-142"
-                  >
-                    <a href="https://www.my-big-toe.com/product-category/paperback-books/">
-                      Paperback Books
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-141"
-                    className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-141"
-                  >
-                    <a href="https://www.my-big-toe.com/product-category/hardback-books/">
-                      Hardback Books
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-389"
-                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-389"
-                  >
-                    <a href="http://www.mbtevents.com/store">Binaural Beats</a>
-                  </li>
-                  <li
-                    id="menu-item-140"
-                    className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-140"
-                  >
-                    <a href="https://www.my-big-toe.com/product-category/reseller-payment/">
-                      Payments &amp; Donations
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-1088"
-                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1088"
-                  >
-                    <a href="https://shop.spreadshirt.com/my-big-toe/">MBT T-Shirts &amp; More</a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                id="menu-item-11"
-                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11"
-              >
-                <a target="_blank" rel="noopener noreferrer" href="https://www.mbtevents.com/">
-                  Events
-                </a>
-              </li>
-              <li
-                id="menu-item-956"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-956"
-              >
-                <a href="https://www.my-big-toe.com/mbt-links/">Links</a>
-              </li>
-              <li
-                id="menu-item-396"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-396"
-              >
-                <a href="https://www.my-big-toe.com/downloads/">Downloads</a>
-                <ul className="sub-menu">
-                  <li
-                    id="menu-item-241"
-                    className="menu-item menu-item-type-post_type menu-item-object-page menu-item-241"
-                  >
-                    <a href="https://www.my-big-toe.com/workshop-gems/">Workshop Slides and Gems</a>
-                  </li>
-                  <li
-                    id="menu-item-397"
-                    className="menu-item menu-item-type-custom menu-item-object-custom menu-item-397"
-                  >
-                    <a href="http://www.my-big-toe.com/uploads/MBT%20Figures%20and%20Equations.pdf">
-                      Figures &amp; Equations
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                id="menu-item-1022"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1022"
-              >
-                <a href="https://www.my-big-toe.com/support/">Support MBT</a>
-              </li>
-              <li
-                id="menu-item-324"
-                className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-324"
-              >
-                <a href="https://www.my-big-toe.com/contact/">Contact</a>
-                <ul className="sub-menu">
-                  <li
-                    id="menu-item-420"
-                    className="menu-item menu-item-type-post_type menu-item-object-page menu-item-420"
-                  >
-                    <a href="https://www.my-big-toe.com/customer-service/">Customer Service</a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                className="menu-item wpmenucartli wpmenucart-display-standard menu-item empty-wpmenucart"
-                id="wpmenucartli"
-              >
-                <a className="wpmenucart-contents empty-wpmenucart" style={{ display: 'none' }}>
-                  &nbsp;
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </Transition>
+        <nav
+          className={`navbar-collapse navbar-ex1-collapse collapse in collapsed ${
+            visible ? 'is-expanded' : ''
+          }`}
+          aria-expanded="true"
+          // style=""
+        >
+          <ul id="menu-main-menu" className="nav navbar-nav navbar-right">
+            <li
+              id="menu-item-6"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-4 current_page_item menu-item-6"
+            >
+              <a href="https://www.my-big-toe.com/" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li
+              id="menu-item-137"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-137"
+            >
+              <a href="https://www.my-big-toe.com/about/">About</a>
+            </li>
+            <li
+              id="menu-item-9"
+              className="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"
+            >
+              <a href="/media/">Videos</a>
+            </li>
+            <li
+              id="menu-item-10"
+              className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10"
+            >
+              <a href="/store1">Store</a>
+              <ul className="sub-menu">
+                <li
+                  id="menu-item-142"
+                  className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-142"
+                >
+                  <a href="https://www.my-big-toe.com/product-category/paperback-books/">
+                    Paperback Books
+                  </a>
+                </li>
+                <li
+                  id="menu-item-141"
+                  className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-141"
+                >
+                  <a href="https://www.my-big-toe.com/product-category/hardback-books/">
+                    Hardback Books
+                  </a>
+                </li>
+                <li
+                  id="menu-item-389"
+                  className="menu-item menu-item-type-custom menu-item-object-custom menu-item-389"
+                >
+                  <a href="http://www.mbtevents.com/store">Binaural Beats</a>
+                </li>
+                <li
+                  id="menu-item-140"
+                  className="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-140"
+                >
+                  <a href="https://www.my-big-toe.com/product-category/reseller-payment/">
+                    Payments &amp; Donations
+                  </a>
+                </li>
+                <li
+                  id="menu-item-1088"
+                  className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1088"
+                >
+                  <a href="https://shop.spreadshirt.com/my-big-toe/">MBT T-Shirts &amp; More</a>
+                </li>
+              </ul>
+            </li>
+            <li
+              id="menu-item-11"
+              className="menu-item menu-item-type-custom menu-item-object-custom menu-item-11"
+            >
+              <a target="_blank" rel="noopener noreferrer" href="https://www.mbtevents.com/">
+                Events
+              </a>
+            </li>
+            <li
+              id="menu-item-956"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-956"
+            >
+              <a href="https://www.my-big-toe.com/mbt-links/">Links</a>
+            </li>
+            <li
+              id="menu-item-396"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-396"
+            >
+              <a href="https://www.my-big-toe.com/downloads/">Downloads</a>
+              <ul className="sub-menu">
+                <li
+                  id="menu-item-241"
+                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-241"
+                >
+                  <a href="https://www.my-big-toe.com/workshop-gems/">Workshop Slides and Gems</a>
+                </li>
+                <li
+                  id="menu-item-397"
+                  className="menu-item menu-item-type-custom menu-item-object-custom menu-item-397"
+                >
+                  <a href="http://www.my-big-toe.com/uploads/MBT%20Figures%20and%20Equations.pdf">
+                    Figures &amp; Equations
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li
+              id="menu-item-1022"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1022"
+            >
+              <a href="https://www.my-big-toe.com/support/">Support MBT</a>
+            </li>
+            <li
+              id="menu-item-324"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-324"
+            >
+              <a href="https://www.my-big-toe.com/contact/">Contact</a>
+              <ul className="sub-menu">
+                <li
+                  id="menu-item-420"
+                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-420"
+                >
+                  <a href="https://www.my-big-toe.com/customer-service/">Customer Service</a>
+                </li>
+              </ul>
+            </li>
+            <li
+              className="menu-item wpmenucartli wpmenucart-display-standard menu-item empty-wpmenucart"
+              id="wpmenucartli"
+            >
+              <a className="wpmenucart-contents empty-wpmenucart" style={{ display: 'none' }}>
+                &nbsp;
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </nav>
   );
